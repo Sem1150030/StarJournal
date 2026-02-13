cf15f3af129ec6d2f06593cf9054a37ddff0d08c
Sem Star <144341688+Sem1150030@users.noreply.github.com>
2026-02-13 15:44:06 +0100

    Added login and logout, also added snackbar for success or error messages. Made a makefile for easy access to commands

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\indexController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [indexController::class, 'index'])->name('index');
 Route::get('/about', [indexController::class, 'about'])->name('about');
+Route::get('auth/login', [AuthController::class, 'login'])->name('login');
+Route::post('auth/login', [AuthController::class, 'authenticate'])->name('authenticate');
 
+Route::middleware(['auth'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
