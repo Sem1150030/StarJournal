8a1da50dffa6d744e1da1b9fafc7b1dc86294c90
Sem Star <144341688+Sem1150030@users.noreply.github.com>
2026-02-13 16:28:29 +0100

    Wrap login and register in guest middleware

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -5,13 +5,16 @@ use App\Http\Controllers\indexController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [indexController::class, 'index'])->name('index');
-Route::get('/about', [indexController::class, 'about'])->name('about');
-Route::get('auth/login', [AuthController::class, 'login'])->name('login');
-Route::post('auth/login', [AuthController::class, 'authenticate'])->name('authenticate');
-Route::get('auth/register', [AuthController::class, 'register'])->name('register');
-Route::post('auth/register', [AuthController::class, 'store'])->name('register.store');
+    Route::get('/', [indexController::class, 'index'])->name('index');
+    Route::get('/about', [indexController::class, 'about'])->name('about');
 
-Route::middleware(['auth'])->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::middleware(['guest'])->group(function () {
+    Route::get('auth/login', [AuthController::class, 'login'])->name('login');
+    Route::post('auth/login', [AuthController::class, 'authenticate'])->name('authenticate');
+    Route::get('auth/register', [AuthController::class, 'register'])->name('register');
+    Route::post('auth/register', [AuthController::class, 'store'])->name('register.store');
 });
+
+    Route::middleware(['auth'])->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    });
